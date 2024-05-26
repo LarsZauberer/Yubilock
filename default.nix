614a6520ce12102fd4d3cb611c33ee7c5438469c
LarsZauberer <wasser.ian@gmail.com>
2024-05-26 23:01:07 +0200

    Manifest variable building

--- default.nix
+++ default.nix
@@ -1,17 +1,19 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.rustPlatform.buildRustPackage {
-  pname = "yubilock";
-  version = "0.1";
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
-  nativeBuildInputs = [
-    pkgs.pkg-config
-    pkgs.pcsclite
-  ];
-  buildInputs = [
-    pkgs.pkg-config
-    pkgs.pcsclite
-  ];
+{pkgs ? import <nixpkgs> {}}: let
+  manifest = (pkgs.lib.importTOML ./Cargo.lock).package;
+in
+  pkgs.rustPlatform.buildRustPackage {
+    pname = manifest.name;
+    version = manifest.version;
+    cargoLock.lockFile = ./Cargo.lock;
+    src = pkgs.lib.cleanSource ./.;
+    nativeBuildInputs = [
+      pkgs.pkg-config
+      pkgs.pcsclite
+    ];
+    buildInputs = [
+      pkgs.pkg-config
+      pkgs.pcsclite
+    ];
 
-  LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-}
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+  }
